d261fae07cce4b29e4acba04ea72e4fb182dc138
Zhaonian Luan <lzn0830@gmail.com>
2019-10-06 20:37:34 -0700

    set up basic grid view

--- eat/lib/categories_screen.dart
+++ eat/lib/categories_screen.dart
@@ -1,17 +1,22 @@
+import 'package:eat/category_item.dart';
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
-
-      ],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return Scaffold(
+      appBar: AppBar(title: const Text("Meal")),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((category) => CategoryItem(category.title, category.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
       ),
     );
   }
