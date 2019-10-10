c7dd29136d97a260ce45bc06e22252bc21c55881
Zhaonian Luan <lzn0830@gmail.com>
2019-10-09 22:44:56 -0700

    more widgets and data in detail page

--- eat/lib/widgets/meal_item.dart
+++ eat/lib/widgets/meal_item.dart
@@ -15,6 +15,34 @@ class MealItem extends StatelessWidget {
       @required this.complexity,
       @required this.affordability});
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        // TODO: Handle this case.
+        return 'Pricey';
+      case Affordability.Luxurious:
+        // TODO: Handle this case.
+        return 'Expensive';
+      default:
+        return 'Unknown';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -40,8 +68,55 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                    bottom: 20,
+                    right: 10,
+                    child: Container(
+                        width: 250,
+                        color: Colors.black54,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                        child: Text(
+                          title,
+                          style: TextStyle(fontSize: 26, color: Colors.white),
+                          softWrap: true,
+                          overflow: TextOverflow.fade,
+                        )))
               ],
-            )
+            ),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.schedule),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text('$duratioen min')
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.work),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text('$complexityText')
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.attach_money),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text('$affordabilityText')
+                        ],
+                      )
+                    ]))
           ],
         ),
       ),
